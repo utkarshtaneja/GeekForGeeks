b964a5a9107efedb6cc226c6762c9763fe1583b4
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-07-28 14:36:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -32,24 +32,27 @@ class Solution{
 	    // Your code goes here
 	    // Using Dp
 	    int mod = 1000000007;
-	    int[][] dp = new int[n + 1][sum + 1];
-	    
-	    for(int i = 0;i <= n;i++){
-	        dp[i][0] = 1;
-	    }
-	    
-	    for(int i = 1;i <= n;i++){
+        
+        int[][] dp = new int[n + 1][sum + 1];
+        
+        for(int i = 0;i <= n;i++){
 	        for(int j = 0;j <= sum;j++){
-	            if(arr[i - 1] <= j){
-	                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - arr[i - 1]] ) % mod;
-	            }
-	            
-	            else{
-	                dp[i][j] = (dp[i - 1][j]) % mod;
-	            }
+	            if(i == 0) dp[i][j] = 0;
+	            if(j == 0) dp[i][j] = 1;
 	        }
 	    }
-	    
-	    return dp[n][sum];
+        
+        for(int i = 1;i <= n;i++){
+            for(int j = 0;j <= sum;j++){
+                if(arr[i - 1] <= j){
+                    dp[i][j] = ( dp[i - 1][j] + dp[i - 1][j - arr[i - 1]] ) % mod;
+                }
+                else{
+                    dp[i][j] = ( dp[i - 1][j] ) % mod;
+                }
+            }
+        }
+        
+        return dp[n][sum];
 	} 
 }
