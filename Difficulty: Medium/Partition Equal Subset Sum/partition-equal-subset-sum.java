5e6fc2365e373d8635ea2eb4ae80c7d70a23baf7
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-08-10 11:27:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -31,17 +31,17 @@ class GFG{
 // User function Template for Java
 
 class Solution{
-    public static boolean subsetSum(int[] arr, int n, int sum){
-
-        boolean[][] dp = new boolean[n + 1][sum + 1];
-        for(int i = 0;i <= n;i++){
+    public static boolean isSubsetSum(int N, int[] arr, int sum){
+        boolean[][] dp = new boolean[N + 1][sum + 1];
+        
+        for(int i = 0;i <= N;i++){
             for(int j = 0;j <= sum;j++){
                 if(i == 0) dp[i][j] = false;
                 if(j == 0) dp[i][j] = true;
             }
         }
         
-        for(int i = 1;i <= n;i++){
+        for(int i = 1;i <= N;i++){
             for(int j = 1;j <= sum;j++){
                 if(arr[i - 1] <= j){
                     dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
@@ -52,7 +52,7 @@ class Solution{
             }
         }
         
-        return dp[n][sum];
+        return dp[N][sum];
     }
     static int equalPartition(int N, int arr[])
     {
@@ -60,12 +60,12 @@ class Solution{
         
         // Using Recursion
         int sum = 0;
-        for(int i = 0;i < N;i++){
-            sum += arr[i];
+        
+        for(int num : arr){
+            sum += num;
         }
         
         if(sum % 2 != 0) return 0;
-        
-        return subsetSum(arr, N, sum / 2) ? 1 : 0;
+        return isSubsetSum(N, arr, sum / 2) ? 1 : 0;
     }
 }
