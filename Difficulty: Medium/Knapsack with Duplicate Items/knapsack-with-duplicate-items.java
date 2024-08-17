51be1b54e2fdc35db0918a44b3f2ff77fa129bcb
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-08-17 12:52:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -36,47 +36,21 @@ class Solution{
     static int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
+        if(N == 0 || W == 0) return 0;
         
-        // Using Recusrsion
-        // if(N == 0 || W == 0) return 0;
-        
-        // if(wt[N - 1] <= W){
-        //     int include = val[N - 1] + knapSack(N, W - wt[N - 1], val, wt);
-        //     int notinclude = knapSack(N - 1, W, val, wt);
-            
-        //     return Math.max(include, notinclude);
-        // }
-        
-        // else{
-        //     return knapSack(N - 1, W, val, wt);
-        // }
-        
-        // Using Memoization
         int[][] dp = new int[N + 1][W + 1];
         
-        for(int i = 0;i <= N;i++){
-            for(int j = 0;j <= W;j++){
-                dp[i][j] = 0;
+        for(int i = 1;i <= N;i++){
+            for(int j = 1;j <= W;j++){
+                if(wt[i - 1] <= j){
+                    dp[i][j] = Math.max(dp[i][j - wt[i - 1]] + val[i - 1] , dp[i - 1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
         
-        return knapSackUtil(N, W, val, wt, dp);
-    }
-    // Memoization Dp
-    public static int knapSackUtil(int N, int W, int val[], int wt[], int[][] dp) {
-        if(N == 0 || W == 0) return 0;
-        
-        if(dp[N][W] != 0) return dp[N][W];
-        if(wt[N - 1] <= W){
-            int include = val[N - 1] + knapSackUtil(N, W - wt[N - 1], val, wt, dp);
-            int notinclude = knapSackUtil(N - 1, W, val, wt, dp);
-            dp[N][W] = Math.max(include, notinclude);
-            return dp[N][W];
-        }
-        
-        else{
-            dp[N][W] = knapSackUtil(N - 1, W, val, wt, dp);
-            return dp[N][W];
-        }
+        return dp[N][W];
     }
 }
