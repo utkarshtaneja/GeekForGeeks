5d585abfe1129e2c236a48b31bfdfa8917428e0b
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-08-20 21:19:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -26,61 +26,46 @@ class GFG {
 
 class Solution {
     // Function to find the length of longest common subsequence in two strings.
-     public static int LCSUsingMemoization(String s1, int n, String s2, int m, int[][] dp){
-        if(n == 0 || m == 0) return 0;
-        
-        if(dp[n][m] != -1) return dp[n][m];
-        
-        if(s1.charAt(n - 1) == s2.charAt(m - 1)){
-            dp[n][m] = LCSUsingMemoization(s1, n - 1, s2, m - 1, dp) + 1;
-            return dp[n][m];
-        }
-        
-        int first = LCSUsingMemoization(s1, n, s2, m - 1, dp);
-        int second = LCSUsingMemoization(s1, n - 1, s2, m, dp);
-        dp[n][m] = Math.max(first, second);
-        return dp[n][m];
-    }
     static int lcs(int n, int m, String str1, String str2) {
         // your code here
         
-        // Using Recursion
+        // Using recursion
         // if(n == 0 || m == 0) return 0;
         
         // if(str1.charAt(n - 1) == str2.charAt(m - 1)){
-        //     return lcs(n - 1, m - 1, str1, str2) + 1;
+        //     return 1 + lcs(n - 1, m - 1, str1, str2);   
         // }
         
-        // int first = lcs(n , m - 1, str1, str2);
-        // int second = lcs(n - 1, m, str1, str2);
-        // return Math.max(first, second);
+        // else{
+        //     return Math.max(lcs(n - 1, m, str1, str2), lcs(n, m - 1, str1, str2));
+        // }
         
         // Using Memoization
         int[][] dp = new int[n + 1][m + 1];
+        
         for(int i = 0;i <= n;i++){
             for(int j = 0;j <= m;j++){
                 dp[i][j] = -1;
             }
         }
         
-        return LCSUsingMemoization(str1, n, str2, m, dp);
+        return LCS_Memo(n, m, str1, str2, dp);
+    }
+    
+    public static int LCS_Memo(int n, int m, String str1, String str2, int[][] dp){
+        if(n == 0 || m == 0) return 0;
         
-        // Using Tabulation
-        // if(n == 0 || m == 0) return 0;
+        if(dp[n][m] != -1) return dp[n][m];
         
-        // int[][] dp = new int[n + 1][m + 1];
+        if(str1.charAt(n - 1) == str2.charAt(m - 1)){
+            dp[n][m] = 1 + LCS_Memo(n - 1, m - 1, str1, str2, dp);
+            return dp[n][m];   
+        }
         
-        // for(int i = 1;i <= n;i++){
-        //     for(int j = 1;j <= m;j++){
-        //         if(str1.charAt(i - 1) == str2.charAt(j - 1)){
-        //             dp[i][j] = dp[i - 1][j - 1] + 1;   
-        //         }
-        //         else{
-        //             dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-        //         }
-        //     }
-        // }
-        
-        // return dp[n][m];
+        else{
+            dp[n][m] = Math.max( LCS_Memo(n - 1, m, str1, str2, dp), 
+                                 LCS_Memo(n, m - 1, str1, str2, dp) );
+            return dp[n][m];
+        }
     }
 }
