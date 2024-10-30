6efb9cf68c5125d1b99ebeeb9145a0e5febd4683
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-10-30 14:48:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,99 +1,60 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class gfg {
-    public static void main(String[] args) throws Exception {
+public class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            int w = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
+            // Read capacity
+            int capacity = Integer.parseInt(br.readLine());
 
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            List<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
+            // Read values
+            String[] valInput = br.readLine().split(" ");
+            int[] val = new int[valInput.length];
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
             }
 
-            int[] val = new int[array.size()];
-            int idx = 0;
-            for (int i : array) val[idx++] = i;
-
-            String lin = br.readLine();
-            String[] toke = lin.split(" ");
-            List<Integer> array1 = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : toke) {
-                array1.add(Integer.parseInt(token));
+            // Read weights
+            String[] wtInput = br.readLine().split(" ");
+            int[] wt = new int[wtInput.length];
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
             }
 
-            int[] wt = new int[array1.size()];
-            idx = 0;
-            for (int i : array1) wt[idx++] = i;
-
-            // calling method knapSack() of class Solution
-            System.out.println(new Solution().knapSack(w, wt, val));
+            // Call the knapSack function and print the result
+            System.out.println(Solution.knapSack(capacity, val, wt));
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    // Function to return max value that can be put in knapsack of capacity W.
-    // public static int KnapSackRecursive(int W, int n, int[] wt, int[] val){
-    //     if(n == 0 || W == 0) return 0;
-        
-    //     if(wt[n - 1] <= W){
-    //         int pick = val[n - 1] + KnapSackRecursive(W - wt[n - 1], n - 1, wt, val);
-    //         int notPick = KnapSackRecursive(W, n - 1, wt, val);
-            
-    //         return Math.max(pick, notPick);
-    //     }
-        
-    //     else{
-    //         return KnapSackRecursive(W, n - 1, wt, val);
-    //     }
-    // }
-    
-    public static int KnapSackMemoize(int W, int n, int[] wt, int[] val, int[][] dp){
+    public static int knapsack(int[] val, int[] wt, int n, int W){
         if(n == 0 || W == 0) return 0;
         
-        if(dp[n][W] != -1) return dp[n][W];
-        
         if(wt[n - 1] <= W){
-            int pick = val[n - 1] + KnapSackMemoize(W - wt[n - 1], n - 1, wt, val, dp);
-            int notPick = KnapSackMemoize(W, n - 1, wt, val, dp);
-            dp[n][W] = Math.max(pick, notPick);
-            return dp[n][W];
+            int pick = val[n - 1] + knapsack(val, wt, n - 1, W - wt[n - 1]);
+            int not_pick = knapsack(val, wt, n - 1, W);
+            
+            return Math.max(pick, not_pick);
         }
         
-        else{
-            dp[n][W] = KnapSackMemoize(W, n - 1, wt, val, dp);
-            return dp[n][W];
-        }
+        return knapsack(val, wt, n - 1, W);
     }
-    static int knapSack(int W, int wt[], int val[]) {
-        // your code here
+    // Function to return max value that can be put in knapsack of capacity.
+    static int knapSack(int capacity, int val[], int wt[]) {
+        // code here
         
-        // Recursive code
-        // int n = wt.length;
-        // return KnapSack(W, n, wt, val);
-        
-        // Memoization
-        int n = wt.length;
-        int[][] dp = new int[n + 1][W + 1];
-        
-        for(int i = 0;i <= n;i++){
-            for(int j = 0;j <= W;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return KnapSackMemoize(W, n, wt, val, dp);
+        // Using recursion
+        int n = val.length;
+        return knapsack(val, wt, n, capacity);
     }
 }
