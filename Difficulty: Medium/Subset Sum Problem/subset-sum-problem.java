843455b6e705d65ae3d8c11ea89f0b2be0c612b0
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2024-10-31 12:15:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -2,28 +2,23 @@
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
+class GFG {
+    public static void main(String args[]) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            int N = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
             String input_line[] = read.readLine().trim().split("\\s+");
-            int arr[]= new int[N];
-            for(int i = 0; i < N; i++)
-                arr[i] = Integer.parseInt(input_line[i]);
+            int N = input_line.length;
+            int arr[] = new int[N];
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(input_line[i]);
             int sum = Integer.parseInt(read.readLine());
 
             Solution ob = new Solution();
-            if(ob.isSubsetSum(N, arr, sum))
-            System.out.println(1);
+            if (ob.isSubsetSum(arr, sum))
+                System.out.println("true");
             else
-            System.out.println(0);
-
-            
+                System.out.println("false");
         }
     }
 }
@@ -31,12 +26,13 @@ class GFG
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution{
-    static Boolean isSubsetSum(int N, int arr[], int sum){
+class Solution {
+
+    static Boolean isSubsetSum(int arr[], int sum) {
         // code here
-        int n = N;
+        int n = arr.length;
         boolean[][] dp = new boolean[n + 1][sum + 1];
         
         for(int i = 0;i <= n;i++){
@@ -47,9 +43,9 @@ class Solution{
         }
         
         for(int i = 1;i <= n;i++){
-            for(int j = 1;j <=  sum;j++){
+            for(int j = 1;j <= sum;j++){
                 if(arr[i - 1] <= j){
-                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
                 }
                 else{
                     dp[i][j] = dp[i - 1][j];
