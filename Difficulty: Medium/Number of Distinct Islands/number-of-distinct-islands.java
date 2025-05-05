d0daea2f462e58a9fdb4e94abb7d633fbc9459da
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-05-06 00:03:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.java
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.java
@@ -26,7 +26,9 @@ class GFG {
             Solution ob = new Solution();
             int ans = ob.countDistinctIslands(grid);
             System.out.println(ans);
-        }
+        
+System.out.println("~");
+}
     }
 }
 // } Driver Code Ends
@@ -35,48 +37,40 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    public static void dfs(int row, int col, int[][] vis, int[][] grid, ArrayList<String> list, int row0, int col0){
-        vis[row][col] = 1;
-        list.add(toString(row - row0, col - col0));
-        
+    public static void dfs(int row, int col, boolean[][] vis, int[][] grid, List<String> list, int row0, int col0){
+        vis[row][col] = true;
+        list.add(toString(row0 - row, col0 - col));
         int n = grid.length;
         int m = grid[0].length;
-        
         int[] dRow = {-1, 0, 1, 0};
         int[] dCol = {0, 1, 0, -1};
-        
         for(int i = 0;i < 4;i++){
             int nRow = row + dRow[i];
             int nCol = col + dCol[i];
             
-            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && vis[nRow][nCol] == 0 && grid[nRow][nCol] == 1){
+            if(nRow >= 0 && nCol >= 0 && nRow < n && nCol < m && !vis[nRow][nCol] && grid[nRow][nCol] == 1){
                 dfs(nRow, nCol, vis, grid, list, row0, col0);
             }
         }
     }
-    
     public static String toString(int row, int col){
         return Integer.toString(row) + " " + Integer.toString(col);
     }
-    
-    public static int countDistinctIslands(int[][] grid) {
+    int countDistinctIslands(int[][] grid) {
         // Your Code here
+        Set<List<String>> set = new HashSet<>();
         int n = grid.length;
         int m = grid[0].length;
-        
-        int[][] vis = new int[n][m];
-        Set<ArrayList<String>> set = new HashSet<>();
-        
+        boolean[][] vis = new boolean[n][m];
         for(int i = 0;i < n;i++){
             for(int j = 0;j < m;j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0){
-                    ArrayList<String> list = new ArrayList<>();
-                    dfs(i, j, vis, grid, list, i, j);
-                    set.add(list);
+                if(!vis[i][j] && grid[i][j] == 1){
+                    List<String> temp = new ArrayList<>();
+                    dfs(i, j, vis, grid, temp, i, j);
+                    set.add(temp);
                 }
             }
         }
-        
         return set.size();
     }
 }
