61fee146920b4aef21eba82b6f42b7a11c55cc6f
Utkarsh Taneja <112970573+utkarshtaneja@users.noreply.github.com>
2025-05-11 23:01:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,119 +1,70 @@
 //{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
+// Initial Template for Java
 import java.io.*;
+import java.util.*;
+
 class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            int[][] edges = new int[E][2];
             for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
             }
+
             Solution obj = new Solution();
-            boolean ans = obj.isCycle(V, adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
+            boolean ans = obj.isCycle(V, edges);
+            System.out.println(ans ? "true" : "false");
+            System.out.println("~");
         }
+        sc.close();
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    // static class Pair{
-    //     int node;
-    //     int src;
+    public static boolean dfs(int curr, boolean[] vis, int parent, ArrayList<ArrayList<Integer>> adj){
+        vis[curr] = true;
         
-    //     public Pair(int node, int src){
-    //         this.node = node;
-    //         this.src = src;
-    //     }
-    // }
-    // public static boolean check(int curr, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
-    //     vis[curr] = true;
-        
-    //     Queue<Pair> q = new LinkedList<>();
-    //     q.add(new Pair(curr, -1));
-        
-    //     while(!q.isEmpty()){
-    //         Pair p = q.poll();
-    //         int node = p.node;
-    //         int src = p.src;
-            
-    //         for(int connected : adj.get(node)){
-    //             if(vis[connected]){
-    //                 if(src != connected){
-    //                     return true;
-    //                 }
-    //             }
-    //             else{
-    //                 q.add(new Pair(connected, node));
-    //                 vis[connected] = true;
-    //             }
-    //         }
-    //     }
-        
-    //     return false;
-    // }
-    // public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-    //     // Code here
-        
-    //     // Using BFS
-    //     boolean[] vis = new boolean[V];
-    //     Arrays.fill(vis, false);
-        
-    //     for(int i = 0;i < V;i++){
-    //         if(!vis[i]){
-    //             if(check(i, vis, adj)){
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
-    
-    public static boolean dfs(int src, int parent, boolean[] vis, ArrayList<ArrayList<Integer>> adj){
-        vis[src] = true;
-        
-        for(int connected : adj.get(src)){
-            if(!vis[connected]){
-                if(dfs(connected, src, vis, adj)){
-                    return true;
-                }
+        for(int val : adj.get(curr)){
+            if(!vis[val]){
+                if(dfs(val, vis, curr, adj)) return true;
             }
-            else if(connected != parent){
+            else if(val != parent){
                 return true;
             }
         }
-        
         return false;
     }
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+    public boolean isCycle(int V, int[][] edges) {
         // Code here
-        // Using DFS
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0;i < V;i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
         boolean[] vis = new boolean[V];
         Arrays.fill(vis, false);
         
         for(int i = 0;i < V;i++){
             if(!vis[i]){
-                if(dfs(i, -1, vis, adj)){
-                    return true;
-                }
-            }
+                if(dfs(i, vis, -1, adj)) return true;
+            }  
         }
+        
         return false;
     }
 }
